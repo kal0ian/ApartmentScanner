4bbeedba01d4ffd808249d1e4daebb9b212580c0
elzaharieva <el.zaharieva28@gmail.com>
2018-01-21 22:04:17 +0200

    Stopwords && Fiend storage fixed

--- src/main/java/bg/uni/sofia/fmi/data/mining/project/lucene/ApartmentDocumentCreator.java
+++ src/main/java/bg/uni/sofia/fmi/data/mining/project/lucene/ApartmentDocumentCreator.java
@@ -16,14 +16,15 @@ public class ApartmentDocumentCreator extends DocumentCreator {
     public Document createDocument(File file) {
         Document document = new Document();
         List<String> fields = parse(file);
-        document.add(new TextField("title", fields.get(0), Field.Store.YES));
-        document.add(new TextField("content", fields.get(1), Field.Store.NO));
-        document.add(new StringField("address", fields.get(2), Field.Store.YES));
-        document.add(new StringField("area", fields.get(3), Field.Store.YES));
-        document.add(new StringField("floor", fields.get(4), Field.Store.YES));
-        document.add(new StringField("construction_type", fields.get(5), Field.Store.YES));
-        document.add(new StringField("telephone", fields.get(6), Field.Store.YES));
-        document.add(new StringField("url", fields.get(7), Field.Store.YES));
+        document.add(new TextField("title", fields.get(0), Field.Store.NO));
+        document.add(new StringField("price", fields.get(1), Field.Store.NO));
+        document.add(new TextField("content", fields.get(2), Field.Store.NO));
+        document.add(new StringField("address", fields.get(3), Field.Store.NO));
+        document.add(new StringField("area", fields.get(4), Field.Store.NO));
+        document.add(new StringField("floor", fields.get(5), Field.Store.NO));
+        document.add(new StringField("construction_type", fields.get(6), Field.Store.NO));
+        document.add(new StringField("telephone", fields.get(7), Field.Store.NO));
+        document.add(new StringField("url", fields.get(8), Field.Store.NO));
         document.add(new StringField("path_to_file", file.getAbsolutePath(), Field.Store.YES));
         return document;
     }
@@ -31,11 +32,10 @@ public class ApartmentDocumentCreator extends DocumentCreator {
     private List<String> parse(File file) {
         List<String> lines = new ArrayList<>();
         String line = "";
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             while ((line = br.readLine()) != null) {
-                lines.add(line);
-                System.out.println(line);
-
+            	lines.add(line);
+                //System.out.println(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
