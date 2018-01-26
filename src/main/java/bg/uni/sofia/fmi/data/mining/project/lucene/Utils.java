dbb9ab076b02fe17dc66f84b6f60c4883a8a9fb1
Kaloyan Spiridonov <kal0ian7@abv.bg>
2018-01-26 18:59:05 +0200

    add bulgarian apartment dictionary

--- src/main/java/bg/uni/sofia/fmi/data/mining/project/lucene/Utils.java
+++ src/main/java/bg/uni/sofia/fmi/data/mining/project/lucene/Utils.java
@@ -1,13 +1,23 @@
 package bg.uni.sofia.fmi.data.mining.project.lucene;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 
 public final class Utils {
     public static final String BULGARIAN_STOPWORDS_FILENAME = "bulgarianST.txt";
-    public Utils() {}
+    public static final String BULGARIAN_APARTMENT_DICTIONARY_FILENAME = "bg_apartment_dictionary";
+    private ClassLoader classLoader;
+    public Utils() {
+        classLoader = getClass().getClassLoader();
+    }
     
-    public File getStopWordsFileFromResources(){
-        ClassLoader classLoader = getClass().getClassLoader();
-        return new File(classLoader.getResource(BULGARIAN_STOPWORDS_FILENAME).getFile());
+    public Reader getStopWordsFileFromResources() throws FileNotFoundException {
+        return new FileReader(new File(classLoader.getResource(BULGARIAN_STOPWORDS_FILENAME).getFile()));
+    }
+
+    public File getApartmentDictionary() throws FileNotFoundException {
+        return new File(classLoader.getResource(BULGARIAN_APARTMENT_DICTIONARY_FILENAME).getFile());
     }
 }
