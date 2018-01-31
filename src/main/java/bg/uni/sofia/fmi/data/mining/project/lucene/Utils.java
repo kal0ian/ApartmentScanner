0759cb6d5510b6d3640a5a5b43834ff9875eec92
Kaloyan Spiridonov <kal0ian7@abv.bg>
2018-01-31 20:07:08 +0200

    add new spellcheck dictionary

--- src/main/java/bg/uni/sofia/fmi/data/mining/project/lucene/Utils.java
+++ src/main/java/bg/uni/sofia/fmi/data/mining/project/lucene/Utils.java
@@ -1,28 +1,28 @@
 package bg.uni.sofia.fmi.data.mining.project.lucene;
 
+import bg.uni.sofia.fmi.data.mining.project.rest.Constants;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
 
 public final class Utils {
-    public static final String BULGARIAN_STOPWORDS_FILENAME = "bulgarianST.txt";
-    public static final String BULGARIAN_APARTMENT_DICTIONARY_FILENAME = "bg_apartment_dictionary";
-    public static final String APARTMENT_DOCUMENTS_DIRECTORY = "apartment_documents";
+
     private ClassLoader classLoader;
     public Utils() {
         classLoader = getClass().getClassLoader();
     }
     
     public Reader getStopWordsFileFromResources() throws FileNotFoundException {
-        return new FileReader(new File(classLoader.getResource(BULGARIAN_STOPWORDS_FILENAME).getFile()));
+        return new FileReader(new File(classLoader.getResource(Constants.BULGARIAN_STOPWORDS_FILENAME).getFile()));
     }
 
     public File getApartmentDictionary() throws FileNotFoundException {
-        return new File(classLoader.getResource(BULGARIAN_APARTMENT_DICTIONARY_FILENAME).getFile());
+        return new File(classLoader.getResource(Constants.BULGARIAN_APARTMENT_DICTIONARY_FILENAME).getFile());
     }
 
     public File getApartmentDocumentsDirectory() throws FileNotFoundException {
-        return new File(classLoader.getResource(APARTMENT_DOCUMENTS_DIRECTORY).getFile());
+        return new File(classLoader.getResource(Constants.APARTMENT_DOCUMENTS_DIRECTORY).getFile());
     }
 }
--- src/main/java/bg/uni/sofia/fmi/data/mining/project/rest/Constants.java
+++ src/main/java/bg/uni/sofia/fmi/data/mining/project/rest/Constants.java
@@ -4,4 +4,7 @@ public final class Constants {
 	public final static int WORD_SUGGESTIONS_COUNT = 5;
 	public final static String INDEX_DIRECTORY = "ApartmentIndex";
 	public final static String SPELLCHECK_INDEX_DIRECTORY = "SpellcheckIndex";
+	public static final String BULGARIAN_STOPWORDS_FILENAME = "bulgarianST.txt";
+	public static final String BULGARIAN_APARTMENT_DICTIONARY_FILENAME = "bg_apartment_dictionary";
+	public static final String APARTMENT_DOCUMENTS_DIRECTORY = "apartment_documents";
 }
